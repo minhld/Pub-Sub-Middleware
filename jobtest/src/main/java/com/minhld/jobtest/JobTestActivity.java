33f03d21e89f0cd2be18fd0e4ad5a7967067e888
Le Dinh Minh <ledinhminh3883@gmail.com>
2016-08-23 18:31:12 -0700

    moved some code from share test to job test

---
+++ jobtest/src/main/java/com/minhld/jobtest/JobTestActivity.java
@@ -0,0 +1,52 @@
+package com.minhld.jobtest;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.minhld.wfd.Utils;
+
+import butterknife.BindView;
+
+public class JobTestActivity extends AppCompatActivity {
+    @BindView(R.id.createGroupBtn)
+    Button createGroupBtn;
+
+    @BindView(R.id.discoverBtn)
+    Button discoverBtn;
+
+    @BindView(R.id.pubBtn)
+    Button pubBtn;
+
+    @BindView(R.id.subBtn)
+    Button subBtn;
+
+    @BindView(R.id.deviceList)
+    ListView deviceList;
+
+    @BindView(R.id.infoText)
+    TextView infoText;
+
+    Handler mainUiHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case Utils.MESSAGE_INFO: {
+                    String strMsg = (String) msg.obj;
+                    UITools.writeLog(JobTestActivity.this, infoText, strMsg);
+                    break;
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jobtest);
+    }
+}
