c8fe4a37a4a5098103e3bce5719a42967b70813a
Le Dinh Minh <ledinhminh3883@gmail.com>
2016-10-17 16:07:18 -0700

    completed loading jar file with bitmap classes

--- dextest/src/main/java/com/minhld/dextest/MainActivity.java
+++ dextest/src/main/java/com/minhld/dextest/MainActivity.java
@@ -1,13 +1,66 @@
 package com.minhld.dextest;
 
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.minhld.jobex.JobDataParser;
+import com.minhld.utils.Utils;
+
+import java.lang.reflect.Method;
+
+import dalvik.system.DexClassLoader;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        loadJarAsync();
+    }
+
+    Bitmap loadedBmp;
+
+    private void loadJarAsync() {
+        new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                try {
+                    loadedBmp = loadJar();
+                    publishProgress();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onProgressUpdate(Object[] values) {
+                // display the bitmap
+                imageView.setImageBitmap(loadedBmp);
+            }
+        }.execute();
+    }
+
+    private Bitmap loadJar() throws Exception {
+        // save the job data to file
+        String objectPath = Utils.getDownloadPath() + "/Job.jar";
+
+        //
+        String dexDir = getDir("dex", 0).getAbsolutePath();
+        ClassLoader parent  = getClass().getClassLoader();
+        DexClassLoader loader = new DexClassLoader(objectPath, dexDir, null, parent);
+        Class dataParserClass = loader.loadClass("com.minhld.jobex.BitmapJobDataParser");
+        JobDataParser dataParser = (JobDataParser) dataParserClass.newInstance();
+
+        String imagePath = Utils.getDownloadPath() + "/mars.jpg";
+        return (Bitmap) dataParser.loadObject(imagePath);
     }
 }
---
+++ dextest/src/main/java/com/minhld/jobex/JobDataParser.java
@@ -0,0 +1,96 @@
+package com.minhld.jobex;
+
+/**
+ * this interface holds an abstraction of data parser. developer needs to override
+ * this method to have the suitable parser for their data
+ *
+ * Created by minhld on 11/23/2015.
+ */
+public interface JobDataParser {
+    /**
+     * get class definition of the data object which you use to exchange
+     * to other devices
+     *
+     * @return
+     */
+    public Class getDataClass();
+
+    /**
+     * read data object from local file
+     *
+     * @param path
+     * @return
+     * @throws Exception
+     */
+    public Object loadObject(String path) throws Exception;
+
+    /**
+     * convert data object to byte array - serialize object to bytes
+     *
+     * @param objData
+     * @return
+     * @throws Exception
+     */
+    public byte[] parseObjectToBytes(Object objData) throws Exception;
+
+    /**
+     * convert from byte array to data object - deserialize byte array to object<br>
+     * some object type cannot be deserialized, for example Bitmap
+     *
+     * @param byteData
+     * @return
+     * @throws Exception
+     */
+    public Object parseBytesToObject(byte[] byteData) throws Exception;
+
+    /**
+     * this function will get one small piece of a data object. the position
+     * of the split depends on its index.<br>
+     *
+     * example: one big bitmap can be splitted into 3 parts, the part with
+     * index 2 will be the third one (the last)
+     *
+     *
+     * @param objData
+     * @param numOfParts
+     * @param index
+     * @return
+     */
+    public Object getPartFromObject(Object objData, int numOfParts, int index);
+    public String getJsonMetadata(Object objData);
+
+    /**
+     * create the placeholder to cumulate the results from other devices
+     * sending back to requester
+     *
+     * @param jsonMetadata
+     * @return
+     */
+    public Object createObjectHolder(String jsonMetadata);
+
+    /**
+     * merge the part data object into the final object.
+     *
+     * @param placeholderObj
+     * @param partObj
+     * @param index
+     * @return
+     */
+    public Object copyPartToHolder(Object placeholderObj, byte[] partObj, int index);
+
+    /**
+     * destroyObject the data object from the memory. If the object is input/output stream
+     * object, then it will be closed. If it is a bitmap, it will be recycled etc...
+     *
+     * @param data
+     */
+    public void destroyObject(Object data);
+
+    /**
+     * check if the data object is really destroyed or not
+     *
+     * @param data
+     * @return
+     */
+    public boolean isObjectDestroyed(Object data);
+}
