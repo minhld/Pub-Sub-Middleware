9623f10d3deaf2e8fd8ce7e7d65b4b12c89d379e
Le Dinh Minh <ledinhminh3883@gmail.com>
2016-12-10 13:28:08 -0700

    added the failure handling project

---
+++ edgehandlingtest/src/main/java/com/minhld/edgehandlingtest/JobSupporter.java
@@ -0,0 +1,22 @@
+package com.minhld.edgehandlingtest;
+
+import android.content.Context;
+
+import com.minhld.jobex.JobDataParser;
+
+/**
+ * Created by minhld on 10/17/2016.
+ */
+
+public class JobSupporter {
+    static JobDataParser dataParser;
+
+    public static void initDataParser(Context c, String jarPath) throws Exception {
+        dataParser = new WordDataParserImpl();
+    }
+
+    public static byte[] getData(String filePath) throws Exception {
+        Object dataObj = dataParser.loadObject(filePath);
+        return dataParser.parseObjectToBytes(dataObj);
+    }
+}
---
+++ edgehandlingtest/src/main/java/com/minhld/edgehandlingtest/UITools.java
@@ -0,0 +1,131 @@
+package com.minhld.edgehandlingtest;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by minhld on 01/28/2016.
+ */
+public class UITools {
+    public static final String GO_IP = "192.168.49.1";
+    public static final SimpleDateFormat SDF = new SimpleDateFormat("MM-dd HH:mm:ss.SSS");
+
+    /**
+     * display confirmation YES/NO
+     *
+     * @param c
+     * @param message
+     * @param listener
+     */
+    public static void showYesNo(Context c, String message, final ConfirmListener listener){
+        AlertDialog.Builder builder = new AlertDialog.Builder(c);
+        builder.setTitle("confirm");
+        builder.setMessage(message);
+
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                if (listener != null) {
+                    listener.confirmed();
+                }
+            }
+        });
+
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    /**
+     * open a dialog to prompt text
+     *
+     * @param c
+     * @param listener
+     */
+    public static void showInputDialog(Context c, final InputDialogListener listener, String... defs) {
+        // get prompts.xml view
+        LayoutInflater layoutInflater = LayoutInflater.from(c);
+        View promptView = layoutInflater.inflate(R.layout.input_dialog, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(c);
+        alertDialogBuilder.setView(promptView);
+
+        final EditText editText = (EditText) promptView.findViewById(R.id.edittext);
+        if (defs.length > 0) editText.setText(defs[0]);
+
+        // setup a dialog window
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        listener.inputDone(editText.getText().toString());
+                    }
+                })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create an alert dialog
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+
+    public interface InputDialogListener {
+        public void inputDone(String resultStr);
+    }
+
+    /**
+     * write the log out to the main screen
+     *
+     * @param c
+     * @param log
+     * @param msg
+     */
+    public static void writeLog(Activity c, final TextView log, final String msg){
+        c.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                log.append(SDF.format(new Date()) + ": " + msg + "\r\n");
+            }
+        });
+    }
+
+    /**
+     * write the logout with prefix and exception
+     *
+     * @param c
+     * @param log
+     * @param prefix
+     * @param e
+     */
+    public static void writeLog(Activity c, final TextView log, final String prefix, final Exception e){
+        c.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                log.append(SDF.format(new Date()) + ": [" + prefix + "] " + e.getMessage() + "\r\n");
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public interface ConfirmListener {
+        public void confirmed();
+    }
+
+}
