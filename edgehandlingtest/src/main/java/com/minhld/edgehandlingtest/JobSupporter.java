7926ca578a9fc2b1161b313f8e8f6bbdd429f639
Le Dinh Minh <ledinhminh3883@gmail.com>
2016-12-10 14:37:54 -0700

    added the failure handling project

--- edgehandlingtest/src/main/java/com/minhld/edgehandlingtest/JobSupporter.java
+++ edgehandlingtest/src/main/java/com/minhld/edgehandlingtest/JobSupporter.java
@@ -3,6 +3,8 @@ package com.minhld.edgehandlingtest;
 import android.content.Context;
 
 import com.minhld.jobex.JobDataParser;
+import com.minhld.jobimpls.*;
+import com.minhld.jobimpls.NetDataParserImpl;
 
 /**
  * Created by minhld on 10/17/2016.
@@ -12,7 +14,7 @@ public class JobSupporter {
     static JobDataParser dataParser;
 
     public static void initDataParser(Context c, String jarPath) throws Exception {
-        dataParser = new WordDataParserImpl();
+        dataParser = new NetDataParserImpl();
     }
 
     public static byte[] getData(String filePath) throws Exception {
